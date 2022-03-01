3e14b9c5c207b5defbfa1fc3c63a24b6a841e605
inbal.levi <inbal.levi@solaredge.com>
2022-03-01 22:42:26 +0200

    [Add] Optional library
    - Can be changed by runing: cmake ../ -DUSE_STATICLIB

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,13 +1,17 @@
 
 #include <iostream>
 #include "config.h"
+#ifdef USE_STATICLIB
 #include <StaticLib.hpp>
+#endif
 
 int main()
 {
     std::cout << " ~~~ Main ~~~" << '\n';
 	std::cout << "compiler version: " << __cplusplus << '\n';
     std::cout << "project name: " << PROJECT_NAME << " version: " << PROJECT_VER << std::endl;
+#ifdef USE_STATICLIB
     StaticLib::StaticLib::Foo1();
+#endif
     return 0;
 }
